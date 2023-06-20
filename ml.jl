eabe438ffab423f851c673e32b62089c377b8953
James Hennessy <james.ryan.hennessy@gmail.com>
2023-06-19 20:29:06 -0700

    Create ml.jl

---
+++ ml.jl
@@ -0,0 +1,33 @@
+using Flux
+using Flux: onehotbatch, onecold, crossentropy, throttle
+
+# Define the neural network architecture
+model = Chain(
+  Dense(784, 32, relu),
+  Dense(32, 10),
+  softmax
+)
+
+# Define the loss function
+loss(x, y) = crossentropy(model(x), y)
+
+# Generate dummy training data
+x_train = rand(Float32, 784, 1000)
+y_train = onehotbatch(rand(1:10, 1000), 1:10)
+
+# Define the optimizer
+optimizer = ADAM(0.01)
+
+# Train the model
+for i in 1:10
+  Flux.train!(loss, Flux.params(model), [(x_train, y_train)], optimizer)
+end
+
+# Generate dummy test data
+x_test = rand(Float32, 784, 100)
+y_test = onehotbatch(rand(1:10, 100), 1:10)
+
+# Evaluate the model
+accuracy(x, y) = mean(onecold(model(x)) .== onecold(y))
+acc = accuracy(x_test, y_test)
+println("Test accuracy: $acc")
