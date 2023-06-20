a13ee92da24ba591b74a9b4cf6cc4b5c8f9a3b83
James Hennessy <james.ryan.hennessy@gmail.com>
2023-06-19 21:03:07 -0700

    Create diff_eq.jl

---
+++ diff_eq.jl
@@ -0,0 +1,19 @@
+using DifferentialEquations
+
+# Define the differential equation
+function example_diffeq!(du, u, p, t)
+    du[1] = -0.5 * u[1]
+end
+
+# Set the initial condition
+u0 = [1.0]
+
+# Set the time span
+tspan = (0.0, 5.0)
+
+# Solve the differential equation
+prob = ODEProblem(example_diffeq!, u0, tspan)
+sol = solve(prob, Tsit5())
+
+# Print the solution
+println("Solution at t = 5.0: ", sol(5.0))
